4e98a656f63efe6b4c3e7c5b8aea50753e652005
Shangni <shangnihu@gmail.com>
2018-08-27 08:57:49 -0400

    Added credit info for APIs

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -10,7 +10,7 @@ app.moviesAPIKey = '0f074982f0e6a999d59865dff2184e86';
 app.moviePage;
 app.moviesGenreIDs = {
     convo: [80, 99, 9648], // crime, documentary, mystery
-    laughs: [35, 12, 18, 10751], // comedy, adventure, drama, family
+    laughs: [35, 12, 18, 28], // comedy, adventure, drama, family
     cuddles: [27, 10749, 53] // horror, romance, thriller
 };
 
@@ -46,7 +46,7 @@ app.getMovies = (userGenre, userRating) => {
         }
     }).then((res) => {
         const totalPages = res.total_pages;
-        const topPopular = Math.floor(totalPages * 0.1);
+        const topPopular = Math.floor(totalPages * 0.2);
         const newPageNumber = app.getRandNum(topPopular)+1;
 
         $.ajax({
